538467a8f5b149a261d9f4fd64fa7818a1322a16
Harsha Pavan <mhemanarasimhaharshapavan@gmail.com>
2026-07-14 12:57:07 +0530

    post with Student

--- src/main/java/com/harsha/demo3/StudentServer/StudentServer.java
+++ src/main/java/com/harsha/demo3/StudentServer/StudentServer.java
@@ -2,6 +2,7 @@ package com.harsha.demo3.StudentServer;
 
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,19 +10,12 @@ public class StudentServer {
     //1. store the student --> post
 
     @PostMapping("/create")
-    public String storeStudent(){
-        return """
-                id : 1
-                name : Harsha Pavan
-                Department : CSE
-                age : 20
-                
-                
-                id : 2
-                name : Harsha
-                Department : CSE
-                age:21
-                """;
+    public String storeStudent(@RequestBody Student student) {
+        int id=student.getId();
+        String name=student.getName();
+        int age=student.getAge();
+        String department=student.getDepartment();
+        return "id : "+id+", name : "+name+", age : "+age+", department : "+department;
     }
     //2. Read the Student with id --> get
 
